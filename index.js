bda1c464e2fd76e147b577f9e31b453eb8b18ee6
Breno Pierin <breno.pierin@darede.com.br>
2021-07-19 15:12:43 -0300

    Primeira parte finalizada

---
+++ index.js
@@ -0,0 +1,23 @@
+import axios from "axios"
+
+axios.get('https://pokeapi.co/api/v2/pokemon/gengar')
+.then(data => {
+    let id = console.log(`Numero: #${data.data.id}`)
+    let name = console.log(`Nome: ${data.data.name.charAt().toUpperCase() + data.data.name.slice(1)}`)
+    let height = console.log(`Altura: ${data.data.height * 10 / 100}m`)
+    let weight = console.log(`Peso: ${data.data.weight * 10 / 100}Kg`)
+    let base_experience = console.log(`XP base: ${data.data.base_experience}`)
+    let ability = data.data.abilities.forEach(e => console.log(`Habilidades: ${e.ability.name}`))
+    let stats = console.log('Stats:') + data.data.stats.forEach(e => console.log(' ' + e.stat.name + ': ' + e.base_stat))
+    let moves = console.log('Moves:') + data.data.moves.forEach(e => console.log(' ' + e.move.name))
+    let especie = console.log('Especie:' + data.data.species.name)
+    axios.get('https://pokeapi.co/api/v2/pokemon-species/94/').then(data => {
+        console.log(' Felicidade base: ' + data.data.base_happiness)
+        console.log(' Taxa de Captura: ' + data.data.capture_rate + '%')
+        console.log(' Cor: ' + data.data.color.name)
+        console.log(' Forma: ' + data.data.shape.name)
+        console.log(' Variações:') + data.data.varieties.forEach(e => console.log(' ' + e.pokemon.name))
+        console.log(' Evolui de: ' + data.data.evolves_from_species.name)
+    })
+    
+})
